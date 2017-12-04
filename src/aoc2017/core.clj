8ab3f8656fb2d4a98ee559cf774f755a9f706320
Arnout Roemers <github@functionalbytes.nl>
2017-12-04 02:03:13 +0100

    Day 3

--- src/aoc2017/core.clj
+++ src/aoc2017/core.clj
@@ -2,28 +2,98 @@
 
 ;;; Day 1 - Inverse Captcha
 
+(defn rotate
+  [n coll]
+  (concat (drop n coll) (take n coll)))
+
+(defn keep*
+  [f coll & colls]
+  (apply sequence (comp (map f) (filter some?)) coll colls))
+
+(defn sum
+  [coll]
+  (reduce + coll))
+
+(defn same
+  [x & ys]
+  (when (every? (partial = x) ys)
+    x))
+
 (defn inverse-captcha
-  [input move]
-  (let [moved (concat (drop move input) (take move input))]
-    (first (reduce (fn [[sum moved] n]
-                     [(cond-> sum (= n (first moved)) (+ n))
-                      (rest moved)])
-                   [0 moved]
-                   input))))
+  [coll move]
+  (->> coll (rotate move) (keep* same coll) sum))
 
 
 ;;; Day 2 - Corruption Checksum
 
 (defn difference
-  [input]
-  (apply - (apply (juxt max min) input)))
+  [coll]
+  (apply - (apply (juxt max min) coll)))
 
 (defn division
-  [input]
-  (first (for [a input b input
+  [coll]
+  (first (for [a coll b coll
                :when (and (not= a b) (= (rem a b) 0))]
            (quot a b))))
 
 (defn spreadsheet-checksum
-  [input operation]
-  (reduce + (map operation input)))
+  [colls operation]
+  (->> colls (map operation) sum))
+
+
+;;; Day 3 - Spiral Memory
+
+(defn squares
+  []
+  (letfn [(squares* [n]
+            (cons (* n n) (lazy-seq (squares* (inc n)))))]
+    (squares* 1)))
+
+(defn between
+  [n coll]
+  (reduce (fn [x y]
+            (if (< x n (inc y))
+              (reduced [x y])
+              y))
+          coll))
+
+(defn spiral-memory
+  [n]
+  (let [[x y]  (between n (filter odd? (squares)))
+        length (long (Math/sqrt y))
+        ring   (quot length 2)
+        pos    (rem (- n x) (dec length))
+        diff   (Math/abs (- pos ring))]
+    (+ ring diff)))
+
+(defn lookups
+  []
+  (letfn [(lookups* [n maximus minus times]
+            (condp = n
+              0       (cons [1 (inc minus)]
+                            (lazy-seq (if (< 0 times)
+                                        (lookups* maximus maximus (+ minus 2) (dec times))
+                                        (lookups* (inc maximus) (inc maximus) (+ minus 2) 1))))
+              1       (cons [1 minus (inc minus)]
+                            (lazy-seq (lookups* 0 maximus minus times)))
+              maximus (cons [1 2 minus (dec minus)]
+                            (lazy-seq (lookups* (dec n) maximus minus times)))
+              (cons [1 minus (inc minus) (dec minus)]
+                    (lazy-seq (lookups* (dec n) maximus minus times)))))]
+    (lookups* 1 1 1 2)))
+
+
+(defn stress-test
+  [n]
+  (loop [cell    14
+         vals    [1 1 2 4 5 10 11 23 25 26 54 57 59]
+         lookups (drop 12 (lookups))]
+    (let [val (reduce (fn [sum minus]
+                        (+ sum (nth vals (dec (- cell minus)))))
+                      0
+                      (first lookups))]
+      (if (< val n)
+        (recur (inc cell)
+               (conj vals val)
+               (rest lookups))
+        vals))))
--- test/aoc2017/core_test.clj
+++ test/aoc2017/core_test.clj
@@ -2,6 +2,27 @@
   (:require [clojure.test :refer (deftest is are)]
             [aoc2017.core :as sut]))
 
+;;; Day 1 - Inverse Captcha
+
+(deftest rotate-test
+  (is (= (sut/rotate 1 [1 1 2 2])
+         [1 2 2 1])))
+
+(deftest keep*-test
+  (is (= (sut/keep* #(when (pos? %1) (+ %1 %2)) [-1 0 1] [2 2 2])
+         [3])))
+
+(deftest sum-test
+  (is (= (sut/sum [1 2 3 4 5]) 15)))
+
+(deftest same-test
+  (are [args result] (= (apply sut/same args) result)
+    [1 1]     1
+    [0 1]     nil
+    [2 2 2]   2
+    [2 2 1]   nil
+    [nil nil] nil))
+
 (deftest inverse-captcha-test
   (are [captcha  move sum] (= (sut/inverse-captcha captcha move) sum)
     [1 1 2 2]         1 3
@@ -14,6 +35,9 @@
     [1 2 3 4 2 5]     3 4
     [1 2 3 1 2 3]     3 12
     [1 2 1 3 1 4 1 5] 4 4))
+
+
+;;; Day 2 - Corruption Checksum
 
 (deftest difference-test
   (are [input result] (= (sut/difference input) result)
